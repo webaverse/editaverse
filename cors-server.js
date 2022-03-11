d91cb33040c785087447f4e8723a194d705138c5
Pankaj Pramanik <pkp2.me2k9@gmail.com>
2022-03-11 22:48:35 +0600

    done with cors anywhere

--- cors-server.js
+++ cors-server.js
@@ -1,4 +1,5 @@
 const cors_proxy = require("cors-anywhere");
+const cors_proxy2 = require("cors-anywhere");
 const fs = require("fs");
 const path = require("path");
 
@@ -6,20 +7,31 @@ const path = require("path");
 const host = process.env.CORS_HOST || "0.0.0.0";
 // Listen on a specific port via the PORT environment variable
 const port = process.env.CORS_PORT || 8080;
+const port2 = process.env.CORS_PORT_TWO || 8081;
 
 const key = fs.readFileSync(path.join(__dirname, "certs", "key.pem"));
 const cert = fs.readFileSync(path.join(__dirname, "certs", "cert.pem"));
 
+cors_proxy2
+  .createServer({
+    originWhitelist: [], // Allow all origins
+    requireHeader: ["origin", "x-requested-with"],
+    removeHeaders: ["cookie", "cookie2"]
+  })
+  .listen(8081, host, function() {
+    console.log("Running CORS Anywhere on " + host + ":" + port2);
+  });
+
 cors_proxy
-    .createServer({
-        originWhitelist: [], // Allow all origins
-        requireHeader: ["origin", "x-requested-with"],
-        removeHeaders: ["cookie", "cookie2"],
-        httpsOptions: {
-            key,
-            cert
-        }
-    })
-    .listen(port, host, function () {
-        console.log("Running CORS Anywhere on " + host + ":" + port);
-    });
+  .createServer({
+    originWhitelist: [], // Allow all origins
+    requireHeader: ["origin", "x-requested-with"],
+    removeHeaders: ["cookie", "cookie2"],
+    httpsOptions: {
+      key,
+      cert
+    }
+  })
+  .listen(port, host, function() {
+    console.log("Running CORS Anywhere on " + host + ":" + port);
+  });
