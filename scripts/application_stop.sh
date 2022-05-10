2fecfcaadc587f8c04185fe62991520a45067f5c
Kinjal Raval <kinjalraval18591@gmail.com>
2022-05-10 14:34:25 +0530

    Add nvm path in script

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 #Stopping existing node servers
+
+#add npm and node to path
+export NVM_DIR="$HOME/.nvm"	
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
+
 echo "Stopping any editaverse node servers"
 pm2 stop editaverse
